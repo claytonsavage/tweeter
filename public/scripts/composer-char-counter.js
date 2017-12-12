cbf9fff95816fecc0e568345d74e5c5bd6197741
claytonsavage <clytnsavage@gmail.com>
2017-12-12 22:07:42 +0000

    set up css for hover over tweet

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -1,16 +1,16 @@
 $( document ).ready(function() {
-    $('.textarea-new-tweet').keyup(function (event) {
+  $('.textarea-new-tweet').keyup(function (event) {
     var maxkeysPressed = 140;
-      var length = $(this).val().length;
-      var length = maxkeysPressed - length;
-      var counter = $(this).parent().children('span.counter');
-      counter.text(length);
-      if (length < 0) {
-        counter.css("color", 'red');
-      } if (length >= 0) {
-        counter.css("color", "black");
-      }
-    });
+    var length = $(this).val().length;
+    length = maxkeysPressed - length;
+    var counter = $(this).parent().children('span.counter');
+    counter.text(length);
+    if (length < 0) {
+      counter.css("color", 'red');
+    } if (length >= 0) {
+      counter.css("color", "black");
+    }
+  });
 
 
 });
